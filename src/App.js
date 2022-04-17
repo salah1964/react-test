21a08ddf3a2ef649191fd409804e3a29777366ef
salah1964 <pro_salah_kalil@hotmail.com>
2022-04-18 00:16:27 +0300

    second change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App= () =>{
            <li>Contact</li>
          </ul>
          </header>   
-         <h1 className='hello'> Hello React</h1>
+         <h1 className='hello'>React App for Training.</h1>
          <div className='pfooter'>
            <h2>Designed By SalahAbuHmaid</h2>
            <p>All Rights Recieved</p>
